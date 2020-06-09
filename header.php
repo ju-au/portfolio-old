15609100c42b37d49e3dd4668aa9fdcddd2a81ae
Junya <ueda.the.tallest.pilot@gmail.com>
2020-06-10 05:36:07 +1000

    fixef if on header in header.php

--- header.php
+++ header.php
@@ -1,7 +1,6 @@
 <header>
   <div id="headerContainer">
-    <!-- <?php if ($_SERVER["REQUEST_URI"] == "/portfolio/index.php") : ?> -->
-    <?php if ($_SERVER["REQUEST_URI"] == "https://junyasportfolio.herokuapp.com/index.php") : ?>
+    <?php if ($_SERVER["REQUEST_URI"] == "/portfolio/index.php" || "https://junyasportfolio.herokuapp.com/index.php") : ?>
 
       <h1>Junya's Portfolio Site</h1>
       <div id="nav-drawer">
