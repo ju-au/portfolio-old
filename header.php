b31e7aeac34e98cccff19e2e0d11bba1ea1315e4
Junya <ueda.the.tallest.pilot@gmail.com>
2020-06-10 19:07:11 +1000

    test

--- header.php
+++ header.php
@@ -1,7 +1,10 @@
 <header>
   <div id="headerContainer">
+    <?php echo ($_SERVER["REQUEST_URI"]) ?>
     <?php if ($_SERVER["REQUEST_URI"] == "https://junyasportfolio.herokuapp.com/index.php") : ?>
-    <?php /* if ($_SERVER["REQUEST_URI"] == "/portfolio/index.php") : */ ?>
+
+    <?php 
+      /* if ($_SERVER["REQUEST_URI"] == "/portfolio/index.php") : */ ?>
 
       <h1>Junya's Portfolio Site</h1>
       <div id="nav-drawer">
